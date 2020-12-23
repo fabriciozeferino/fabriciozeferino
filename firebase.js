1e3e4ab7719dc390bf802eb5a7131320a284e648
Fabricio Zeferino <fabriciozeferino84@gmail.com>
2020-12-23 21:29:47 +0000

    "commit before deploy"

--- firebase.js
+++ firebase.js
@@ -1,8 +1,5 @@
-// import * as firebase from 'firebase/app'
-// import 'firebase/auth'
-// import 'firebase/firestore'
-
-import firebase from 'firebase'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 // firebase init - add your own config here
 const firebaseConfig = {
@@ -17,22 +14,11 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig)
 
-// utils
 const db = firebase.firestore()
-const auth = firebase.auth()
 
-// collection references
 const usersCollection = db.collection('users')
-const postsCollection = db.collection('posts')
-const commentsCollection = db.collection('comments')
-const likesCollection = db.collection('likes')
 
-// export utils/refs
 export {
   db,
-  auth,
   usersCollection,
-  postsCollection,
-  commentsCollection,
-  likesCollection
 }
