62d5fea000408b7dcfd9f81bf8c5333d302c4aea
Fabricio Zeferino <fabriciozeferino84@gmail.com>
2021-03-07 21:34:20 +0000

    "commit before deploy"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   rules: {
-    indent: ['error', 2, { ignoredNodes: ['TemplateLiteral'] }],
-    'template-curly-spacing': 'off',
-    quotes: ['warn', 'single'],
-    semi: ['warn', 'never'],
-    'comma-dangle': ['warn', 'always-multiline'],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    indent: ["error", 2, { ignoredNodes: ["TemplateLiteral"] }],
+    "template-curly-spacing": "off",
+    quotes: ["warn", "single"],
+    semi: ["warn", "never"],
+    "comma-dangle": ["warn", "always-multiline"],
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
-}
+        jest: true,
+      },
+    },
+  ],
+};
