99ecf5a9ef150a218226ba4638536cb477eda102
Fabricio Zeferino <fabriciozeferino84@gmail.com>
2021-03-10 20:17:31 +0000

    "commit before deploy"

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === 'production' ? '/fabriciozeferino/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
 
   chainWebpack: config => {
     const svgRule = config.module.rule('svg')
